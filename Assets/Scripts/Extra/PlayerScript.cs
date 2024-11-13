9f8b97e52dc24f40330d11a60c2453951dab0246
Anton Borisov <antonimstudio@gmail.com>
2024-11-14 00:30:27 +0800

    No Android

--- Assets/Scripts/Extra/PlayerScript.cs
+++ Assets/Scripts/Extra/PlayerScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using Unity.Android.Types;
 using System.Runtime.CompilerServices;
 
 public class PlayerScript : MonoBehaviour
